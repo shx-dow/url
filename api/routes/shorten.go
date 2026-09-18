d39ef888b8f0c3304646284548253a31176a8948
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-09-19 00:09:56 +0530

    feat(routes): implement URL shortening logic
    - Add URL validation and domain filtering to prevent internal service abuse
    - Ensure consistent URL formatting by enforcing HTTP protocol
    - Integrate helper functions to sanitize and verify user-provided URLs

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,10 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/ipfs/boxo/ipld/unixfs/importer/helpers"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +18,26 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	RateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// rate limiting
+
+	// check for the input for actual url
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	}
+
+	// domain error checking
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "you cannot hack it ;)"})
+	}
+	// enforce https, ssl
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
