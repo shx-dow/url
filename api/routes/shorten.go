7d3d506d17192b416e079ed054dc9de613c29879
shx-dow <83426772+shx-dow@users.noreply.github.com>
2026-09-20 00:01:26 +0530

    feat(api): implement URL shortening logic
    - Add UUID generation for random short URL creation
    - Implement custom short URL support with collision detection
    - Configure default expiration time for shortened URLs
    - Integrate Redis storage for persistence of URL mappings

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -4,9 +4,11 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"uuid"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/shx-dow/url/database"
 	"github.com/shx-dow/url/helpers"
 )
@@ -63,6 +65,36 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 	// enforce https, ssl
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "custom url already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "unable to connect to the server",
+		})
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 }
